daffa1a9602f229d33c0bb8d681749ceaac2ac26
Leoni Voulgaris <lv383@bath.ac.uk>
2025-08-17 11:38:13 +0100

    added rest of views

--- cookies/api/urls.py
+++ cookies/api/urls.py
@@ -1,8 +1,9 @@
-from .views import ProductView
-from .views import CartItemView
+from .views import ProductView, CartItemView, OrderView, OrderItemView
 from django.urls import path
 
 urlpatterns = [
     path('Product', ProductView.as_view()),
-    path('CartItem', CartItemView.as_view()),  # Include the API URLs
+    path('CartItem', CartItemView.as_view()), 
+    path('Order', OrderView.as_view()),
+    path('OrderItem', OrderItemView.as_view()), # Include the API URLs
 ]
