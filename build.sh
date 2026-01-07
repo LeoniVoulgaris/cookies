ba053124bac62206e3259655f3171c727d3f4dcb
Leoni Voulgaris <leonivou@gmail.com>
2026-01-07 14:47:18 +0000

    changed build

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env sh
 set -o errexit
 
+# Ensure the project root is in PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+# Tell Django where to find settings
+export DJANGO_SETTINGS_MODULE=cookies.settings
+
+# Activate virtual environment (if using Render's default venv path)
+if [ -f "./.venv/bin/activate" ]; then
+    source ./.venv/bin/activate
+fi
+
+# Upgrade pip to latest version
+pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
 
