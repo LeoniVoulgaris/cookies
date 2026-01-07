362d0b56035fffc3558b519d3a1a270af694ef92
Leoni Voulgaris <leonivou@gmail.com>
2026-01-07 14:12:19 +0000

    changed build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
+python manage.py migrate
