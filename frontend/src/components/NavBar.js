afcdad0d1c5c5df1841b4be8e95f0d404eb384b8
Leoni Voulgaris <leonivou@gmail.com>
2026-03-12 16:24:46 +0000

    Increase logo size in NavBar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -29,7 +29,7 @@ const NavBar = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center">
-              <img src={logo} alt="Posted Cookies" className="h-12 w-auto" />
+              <img src={logo} alt="Posted Cookies" className="h-20 w-auto" />
             </div>
 
             <nav className="hidden md:flex items-center gap-8">
